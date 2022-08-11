87d81ae42109394a32d0315c7644d332c4f0288e
triest <triest21@gmail.com>
2022-08-11 16:17:11 +0300

    Add README.md

--- app/Console/Commands/CustomersImportCommand.php
+++ app/Console/Commands/CustomersImportCommand.php
@@ -6,6 +6,7 @@ use App\Exports\CustomersExport;
 use App\Imports\CustomersImport;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
 use Maatwebsite\Excel\Facades\Excel;
 use Maatwebsite\Excel\Validators\ValidationException;
 
@@ -18,6 +19,8 @@ class CustomersImportCommand extends Command
      */
     protected $signature = 'customers:import';
 
+    private $filename = 'РНР_random.csv';
+
     /**
      * The console command description.
      *
@@ -28,10 +31,15 @@ class CustomersImportCommand extends Command
 
     public function handle()
     {
+        $out = new \Symfony\Component\Console\Output\ConsoleOutput();
         $importer = new CustomersImport();
-        $importer->import(storage_path('РНР_random.csv'));
+        if (!Storage::disk('public')->exists($this->filename)) {
+            $out->writeln("file " . $this->filename . ' not found');
+            return;
+        }
+        $importer->import(Storage::disk('public')->path($this->filename));
         $filename = 'customers_error.xlsx';
-        $file = Excel::store(new CustomersExport($importer->getImportedCount()), $filename);
+        $file = Excel::store(new CustomersExport($importer->getImportedCount()), 'public/'.$filename);
         $path = substr(storage_path($file), 0, -1) . $filename;
         $out = new \Symfony\Component\Console\Output\ConsoleOutput();
         $out->writeln($path);
